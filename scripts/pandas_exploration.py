09dea4db5128527ee6e0e1149c8671d190f9fec4
podopie <epodojil@gmail.com>
2015-02-03 00:52:52 -0500

    add script from class

---
+++ scripts/pandas_exploration.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+header = [
+    'mpg', 'cylinders', 'displacement',
+    'horsepower', 'weight', 'acceleration',
+    'model year', 'origin', 'car name'
+]
+
+autompg = pd.read_csv(url, delimiter='\s+', header=0, na_values='?')
+autompg.columns = header
+
+years = [70, 82]
+for year in years:
+    print autompg[autompg['model year'] == year].describe()
+
+for i in autompg['model year'].unique():
+    autompg[autompg['model year'] == i] = autompg[autompg['model year'] == i].fillna(autompg[autompg['horsepower'].notnull()].mean())
+
+autompg['manufacturer'] = autompg['car name'].apply(lambda x: x.split()[0])
+
+print autompg[autompg['horsepower'].isnull()]
